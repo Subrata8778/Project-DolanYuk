6ec52bcb081dd55a3663bec9a8c2d1827ddadd8d
alvinfersus <alvinfersus@gmail.com>
2024-01-11 20:29:14 +0700

    Feat: Logout

--- dolan_yuk/lib/screen/profile.dart
+++ dolan_yuk/lib/screen/profile.dart
@@ -13,9 +13,7 @@ class MyProfile extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: ProfileScreen(),
-      routes: {
-        
-      },
+      routes: {},
     );
   }
 }
@@ -26,7 +24,6 @@ class ProfileScreen extends StatefulWidget {
     return _ProfileScreenState();
   }
 }
-
 
 class _ProfileScreenState extends State<ProfileScreen> {
   String _nama = '';
@@ -48,9 +45,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Future<String> fetchData() async {
     final response = await http.post(
         Uri.parse("https://ubaya.me/flutter/160420002/DolanYuk/selectUser.php"),
-        body: {
-          'users_id': userId.toString()
-        });
+        body: {'users_id': userId.toString()});
     if (response.statusCode == 200) {
       return response.body;
     } else {
@@ -70,11 +65,17 @@ class _ProfileScreenState extends State<ProfileScreen> {
     });
   }
 
+  Future<void> doLogOut() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("user_id", "");
+    main();
+  }
+
   @override
   void initState() {
     super.initState();
     Future<String> userOnly = checkUser();
-    userOnly.then((value){
+    userOnly.then((value) {
       userId = value;
       bacaData();
     });
@@ -84,7 +85,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     final response = await http.post(
         Uri.parse("https://ubaya.me/flutter/160420002/DolanYuk/updateUser.php"),
         body: {
-          'nama': _namaController.text, 
+          'nama': _namaController.text,
           'photo': _photoController.text,
           'id': userId,
         });
@@ -107,78 +108,92 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          height: 500,
-          margin: EdgeInsets.all(20),
-          padding: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30)),
-              border: Border.all(width: 1),
-              color: Colors.white,
-              boxShadow: [BoxShadow(blurRadius: 20)]),
-          child: Column(children: [
-          Container(
-            width: 150,
-            height: 150,
-            child: ClipOval(
-              child: Image.network(
-                _photoController.text,
-                width: 150,
-                height: 150,
-                fit: BoxFit.cover,
-              ),
+      height: 700,
+      margin: EdgeInsets.all(20),
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+          border: Border.all(width: 1),
+          color: Colors.white,
+          boxShadow: [BoxShadow(blurRadius: 20)]),
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              'Tulisan Tombol',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                doLogOut();
+              },
+            ),
+          ],
+        ),
+        Container(
+          width: 150,
+          height: 150,
+          child: ClipOval(
+            child: Image.network(
+              _photoController.text,
+              width: 150,
+              height: 150,
+              fit: BoxFit.cover,
             ),
           ),
-          Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: _namaController,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Nama Lengkap',
-                    hintText: 'Masukkan nama lengkap anda disini'),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: TextField(
+            controller: _namaController,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Nama Lengkap',
+                hintText: 'Masukkan nama lengkap anda disini'),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: TextField(
+            controller: _emailController,
+            enabled: false,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+                hintText: 'Masukkan email Anda'),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          //padding: EdgeInsets.symmetric(horizontal: 15),
+          child: TextField(
+            controller: _photoController,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Photo',
+                hintText: 'Masukkan url Photo Profile Anda'),
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.all(10),
+            child: Container(
+              height: 50,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: ElevatedButton(
+                onPressed: () {
+                  doUpdate();
+                },
+                child: Text(
+                  'Simpan',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: _emailController,
-                enabled: false,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Masukkan email Anda'),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                controller: _photoController,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Photo',
-                    hintText: 'Masukkan url Photo Profile Anda'),
-              ),
-            ),
-          Padding(
-                padding: EdgeInsets.all(10),
-                child: Container(
-                  height: 50,
-                  width: 300,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      doUpdate();
-                    },
-                    child: Text(
-                      'Simpan',
-                      style: TextStyle(color: Colors.white, fontSize: 25),
-                    ),
-                  ),
-                )),
-          ]),
-        ));
+            )),
+      ]),
+    ));
   }
 }
